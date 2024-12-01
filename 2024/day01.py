5dc9d7c928cc5c8787ad27ff121b572b3415744c
Olaf Brandt <olaf.brandt@gmail.com>
2024-12-01 13:29:38 -0800

    docstrings. gitignore.

--- 2024/day01.py
+++ 2024/day01.py
@@ -1,9 +1,12 @@
-#pylint: disable=missing-module-docstring, missing-function-docstring
+""" Module for Advent of Code Day 1.
+    https://adventofcode.com/2024/day/1
+"""
 import sys
 from collections import Counter
 import numpy as np
 
 def main(src_file):
+    """Read two colums of integers and compute total distance and similarity scores."""
     # Read all numbers
     all_nums = np.fromstring(src_file.read(), dtype=int, sep=' ')
 
@@ -11,11 +14,11 @@ def main(src_file):
     left_nums = all_nums[0::2]
     right_nums = all_nums[1::2]
 
-    # Use Counter to count occurrences in the right list
-    right_counts = Counter(right_nums)
-
     # Calculate distances between sorted lists
     distances = np.abs(np.sort(left_nums) - np.sort(right_nums))
+
+    # Use Counter to count occurrences in the right list
+    right_counts = Counter(right_nums)
 
     # Calculate similarity score using numpy arrays
     right_counts_array = np.array([right_counts[x] for x in left_nums])
@@ -32,4 +35,5 @@ if __name__ == "__main__":
         with open(sys.argv[1], mode='r', encoding="utf-8") as file:
             main(file)
     else:
+        # Read from stdin
         main(sys.stdin)
