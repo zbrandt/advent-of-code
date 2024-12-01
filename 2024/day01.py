06da8c17accf75ccd6f97fadfa5a2d4e85a1f324
Olaf Brandt <olaf.brandt@gmail.com>
2024-11-30 22:46:09 -0800

    my answer for Day 1

---
+++ 2024/day01.py
@@ -0,0 +1,13 @@
+import sys
+from collections import Counter
+
+all_nums = list(map(int,sys.stdin.read().split()))
+left_nums  = all_nums[0::2]
+right_nums = all_nums[1::2]
+right_counts = Counter(right_nums)
+
+distances = [abs(x-y) for x,y in zip(sorted(left_nums), sorted(right_nums))]
+similarity = [x * right_counts[x] for x in left_nums]
+
+print (f'Part 1: {sum(distances)  = }')
+print (f'Part 2: {sum(similarity) = }')
