e2c7fbff785ba7f346c1df49a819fbb0d6c3f7ec
Olaf Brandt <olaf.brandt@gmail.com>
2024-12-03 10:08:35 -0800

    nits

--- 2024/day03.py
+++ 2024/day03.py
@@ -3,8 +3,8 @@ import sys, re
 def main(farg):
     enabled = True
     total1 = total2 = 0
-    for a, b, sdo, sdont in re.findall(r"mul\((\d+)\,(\d+)\)|(do\(\))|(don't\(\))", farg.read()):
-        if sdo or sdont:
+    for a, b, do, dont in re.findall(r"mul\((\d+)\,(\d+)\)|(do\(\))|(don't\(\))", farg.read()):
+        if do or dont:
             enabled = not sdont
         else:
             x = int(a) * int(b)
@@ -14,4 +14,5 @@ def main(farg):
     print (f'Part 2: {total2}')
 
 if __name__ == "__main__":
-    main(open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin)
+    main(open(sys.argv[1], encoding="utf-8") if len(sys.argv) > 1 else sys.stdin)
+    
