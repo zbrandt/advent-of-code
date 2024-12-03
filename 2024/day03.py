61cfb10e1527e470d2762eb25c0fe122e896f0ef
Olaf Brandt <olaf.brandt@gmail.com>
2024-12-02 22:32:11 -0800

    further simplification

--- 2024/day03.py
+++ 2024/day03.py
@@ -1,23 +1,17 @@
 import sys, re
-from operator import mul
 
-def main(farg):    
+def main(farg):
     enabled = True
     total1 = total2 = 0
-    for smul, sdo, sdont in re.findall(r"(mul\(\d+\,\d+\))|(do\(\))|(don't\(\))", farg.read()):
+    for a, b, sdo, sdont in re.findall(r"mul\((\d+)\,(\d+)\)|(do\(\))|(don't\(\))", farg.read()):
         if sdo or sdont:
-            enabled = bool(sdo)
+            enabled = not sdont
         else:
-            x = eval(smul)
+            x = int(a) * int(b)
             total1 += x
             total2 += x * enabled
     print (f'Part 1: {total1}')
     print (f'Part 2: {total2}')
 
 if __name__ == "__main__":
-    arg = sys.stdin
-    if len(sys.argv) > 1:
-        with open(sys.argv[1], "r", encoding="utf-8") as arg:
-            main(arg)
-    else:
-        main(arg)
+    arg = open(sys.argv[1]) if len(sys.argv) > 1 else sys.stdin
