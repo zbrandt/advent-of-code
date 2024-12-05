da0bb931d5836fe653c9a874db23a6a8f832e989
Olaf Brandt <olaf.brandt@gmail.com>
2024-12-04 18:33:32 -0800

    new & more terse approach

--- 2024/day04.py
+++ 2024/day04.py
@@ -1,52 +1,21 @@
 import sys, re
+from itertools import product
+from collections import defaultdict
 
-directions = [[1,0],[-1,0],[0,1],[0,-1],[1,1],[1,-1],[-1,1],[-1,-1]]
+dirs = list(product([-1, 0, 1], repeat=2))
 
-def xmas(grid, row, col, dir=None) -> int:
-    if dir == None:
-        return sum(xmas(grid, row, col, dir) for dir in directions)
-    match = 'XMAS'
-    while match:
-        ch = grid[row][col]
-        if ch != match[0]:
-            return 0
-        #print (f'xmas(grid, {row=}, {col=}, {dir=}, {ch=})')
-        match = match[1:]
-        row,col = row+dir[0], col+dir[1]
-    return 1
+grid = None
+def main2(fname):
+    target = list('XMAS')
+    data = fname.read().strip()
+    ddgrid = defaultdict(str,{(i,j): cell for i,row in enumerate(data.split()) for j,cell in enumerate(list(row)) })
+    width = max(x[1] for x in ddgrid.keys())
+    xmas_count = sum([[ddgrid[(k[0] + i*d[0],k[1] + i*d[1])] for i in range(len(target))] == target for d in dirs for k in list(ddgrid.keys())])
 
-def x_mas(grid, row, col) -> int:
-    if (grid[row][col] == 'A' and
-        grid[row+1][col+1] in 'MS' and
-        grid[row+1][col-1] in 'MS' and
-        grid[row-1][col-1] in 'MS' and
-        grid[row-1][col+1] in 'MS' and
-        grid[row-1][col-1] != grid[row+1][col+1] and
-        grid[row-1][col+1] != grid[row+1][col-1]):
-        #print (f'grid[{row}][{col}]')
-        return 1
-    return 0
+    m = re.findall(f'(?s)(?=(M|S).(M|S).{{{width}}}A.{{{width}}}(M|S).(M|S))', data)
 
-def main(fname):
-    grid = [list(x) for x in fname.read().strip().split()]
-
-    xgrid = []
-    xgrid.append(['.'] * (len(grid[0]) + 2))
-    for row in grid:
-        xgrid.append(['.'] + row + ['.'])
-    xgrid.append(['.'] * (len(grid[0]) + 2))
-
-    for row in xgrid:
-        print (''.join(row))
-
-    xmas_count = 0
-    x_mas_count = 0
-    for row,_ in enumerate(xgrid):
-        for col,_ in enumerate(_):
-            xmas_count += xmas(xgrid, row, col)
-            x_mas_count += x_mas(xgrid, row, col)
-    print (f'Part 1: {xmas_count=}')
-    print (f'Part 2: {x_mas_count=}')
+    print(f'Part 1: {xmas_count}')
+    print(f'Part 2: {sum(x[0] != x[3] and x[1] != x[2] for x in m)}')
 
 if __name__ == "__main__":
-    main(open(sys.argv[1], encoding="utf-8") if len(sys.argv) > 1 else sys.stdin)
+    main2(open(sys.argv[1], encoding="utf-8") if len(sys.argv) > 1 else sys.stdin)
