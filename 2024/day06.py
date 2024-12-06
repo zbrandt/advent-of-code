c66825d5e867984e015c811f464fcbd6fda2f36b
Olaf Brandt <olaf.brandt@gmail.com>
2024-12-06 12:27:49 -0800

    more tidying. and progress bar.

--- 2024/day06.py
+++ 2024/day06.py
@@ -2,7 +2,7 @@
     https://adventofcode.com/2024/day/6
 """
 import sys
-
+from rich.progress import track
 class GuardGallivant:
     dirs = ((-1,0), (0,1), (1,0),(0,-1))
 
@@ -28,62 +28,60 @@ class GuardGallivant:
 
     def walk_grid(self):
         r,c = self.start
-        dir = 0
-        while self.valid_pos([r,c]):
+        direction = 0
+        while self.valid_pos((r,c)):
             self.grid[r][c] = 'X'
             self.xes.append((r,c))
             while True:
-                rx,cx = r + self.dirs[dir][0], c + self.dirs[dir][1]
+                rx,cx = r + self.dirs[direction][0], c + self.dirs[direction][1]
                 if self.valid_pos([rx,cx]) and self.grid[rx][cx] == '#':
-                    dir = (dir + 1) % 4
+                    direction = (direction + 1) % 4
                     continue
                 break
             r,c = rx,cx
     
     def has_loop(self) -> bool:
-        gp = self.start
-        dir = 0
+        r,c = self.start
+        direction = 0
         steps = {}
-        while self.valid_pos(gp):
+        while self.valid_pos((r,c)):
             while True:
-                r,c = gp[0] + self.dirs[dir][0], gp[1] + self.dirs[dir][1]
-                if (r,c) in steps:
-                    if steps[(r,c)] == dir:
+                rx,cx = r + self.dirs[direction][0], c + self.dirs[direction][1]
+                
+                # check for loop
+                if (rx,cx) in steps:
+                    if steps[(rx,cx)] == direction:
                         return True
-                steps[(r,c)] = dir
-                if self.valid_pos([r,c]) and (self.grid[r][c] == '#' or self.grid[r][c] == 'O'):
-                    dir = (dir + 1) % 4
+                steps[(rx,cx)] = direction
+
+                if self.valid_pos([rx,cx]) and (self.grid[rx][cx] == '#' or self.grid[rx][cx] == 'O'):
+                    direction = (direction + 1) % 4
                     continue
                 break
-            gp = [r,c]
+            r,c = rx,cx
         return False
 
-    def find_loops(self) -> None:
-        for r in range(self.dims[0]):
-            for c in range(self.dims[1]):
-                ch = self.grid[r][c]
-                if ch == 'X':
-                    self.grid[r][c] = 'O'
-                    if self.has_loop():
-                        self.obstacles.append((r,c))
-                    self.grid[r][c] = '.'
+    def find_loops(self):
+        for r,c in track(self.xes, description="Find loops..."):
+            self.grid[r][c] = 'O'
+            if self.has_loop():
+                self.obstacles.append((r,c))
+            self.grid[r][c] = '.'
 
+    def add_start_and_obstacles(self):
         for obstacle in self.obstacles:
             self.grid[obstacle[0]][obstacle[1]] = 'O'
+        self.grid[self.start[0]][self.start[1]] = '^'
 
 def main(fname):
-
     gg = GuardGallivant(fname)
 
     gg.walk_grid()
-    print (f'Part 1: {len(gg.xes)}')
-    #gg.pp()
-
     gg.find_loops()
+    gg.add_start_and_obstacles()
+    gg.pp()
+    print (f'Part 1: {len(gg.xes)}')
     print (f'Part 2: {len(gg.obstacles)}')
-    #gg.pp()
-
-    return
 
 if __name__ == "__main__":
     main(open(sys.argv[1], encoding="utf-8") if len(sys.argv) > 1 else sys.stdin)
