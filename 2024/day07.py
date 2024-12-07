241d25e8f368e45569f3d08e3448c0a66e33aef8
Olaf Brandt <olaf.brandt@gmail.com>
2024-12-06 23:14:09 -0800

    rename to compute

--- 2024/day07.py
+++ 2024/day07.py
@@ -7,19 +7,19 @@ from operator import add, mul
 def cat(x,y):
     return int(str(x) + str(y))
 
-def solve(target, numbers, ops, running = 0, op = add) -> int:
+def compute(target, numbers, ops, running = 0, op = add) -> int:
     running = op(running, numbers[0])
     if running > target:
         return []
     if len(numbers) == 1:
         return [running]
-    return sum([solve(target, numbers[1:], ops, running, op) for op in ops],[])
+    return sum([compute(target, numbers[1:], ops, running, op) for op in ops],[])
 
 def main(fname):
     targets = {int(k) : list(map(int, v.split())) for k,v in re.findall(r'(\d+)\s*:\s*(\d.*)', fname.read())}
 
-    sum1 = [k*int(k in solve(k, v, (add, mul))) for k,v in targets.items()]
-    sum2 = [k*int(k in solve(k, v, (add, mul, cat))) for k,v in targets.items()]
+    sum1 = [k*int(k in compute(k, v, (add, mul))) for k,v in targets.items()]
+    sum2 = [k*int(k in compute(k, v, (add, mul, cat))) for k,v in targets.items()]
 
     print (f'Part 1: {sum(sum1)}')
     print (f'Part 2: {sum(sum2)}')
