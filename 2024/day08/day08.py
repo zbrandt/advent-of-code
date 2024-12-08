c30b8dce4605f08806142ea37631d866b7e1f46b
Olaf Brandt <olaf.brandt@gmail.com>
2024-12-07 23:57:20 -0800

    oo

--- 2024/day08/day08.py
+++ 2024/day08/day08.py
@@ -1,44 +1,55 @@
 """ Module for Advent of Code Day 8.
     https://adventofcode.com/2024/day/8
 """
-# pylint: disable=line-too-long, missing-function-docstring
+# pylint: disable=line-too-long, missing-function-docstring, missing-class-docstring
 import sys
 from collections import defaultdict
 from itertools import combinations
 import numpy as np
 
+class CityMap:
+
+    def pp(self) -> None:
+        print (f'City Map {self.w}x{self.h}')
+        print ('\n'.join(''.join(ch for ch in row) for row in self.grid))
+
+    def clip_to_city_limits(self, nodes):
+        return np.unique(nodes[(nodes[:, 0] >= 0) & (nodes[:, 0] < self.h) &
+                               (nodes[:, 1] >= 0) & (nodes[:, 1] < self.w)], axis=0)
+
+    def mark_antinodes(self, nodes):
+        for p in nodes:
+            if self.grid[p[0]][p[1]] == '.':
+                self.grid[p[0]][p[1]] = '#'
+
+    def __init__(self, fname):
+        data = fname.read().strip()
+        self.grid = [list(row) for row in data.split()]
+        self.h = len(self.grid)
+        self.w = len(self.grid[0])
+        self.freqs = defaultdict(list)
+        for r,row in enumerate(self.grid):
+            for c,cell in enumerate(row):
+                if cell != '.' and cell != '#':
+                    self.freqs[cell].append(np.array([r,c]))
+
+        self.antinodes = np.array([p[1] + ((p[1] - p[0]) * mults) for v in self.freqs.values() for p in combinations(v,2) for mults in (1,-2)])
+        self.antinodes = self.clip_to_city_limits(self.antinodes)
+
+        diag = self.w + self.h
+        self.antinodes2 = np.array([p[1] + ((p[1] - p[0]) * mults) for v in self.freqs.values() for p in combinations(v,2) for mults in range(-diag,diag)])
+        self.antinodes2 = self.clip_to_city_limits(self.antinodes2)
+
+
 def main(fname) -> None:
 
-    def pp_map() -> None:
-        print (f'City Map {len(citymap[0])}x{len(citymap)}')
-        print ('\n'.join(''.join(ch for ch in row) for row in citymap))
+    city = CityMap(fname)
 
-    def clip_to_city_limits(nodes):
-        return np.unique(nodes[(nodes[:, 0] >= 0) & (nodes[:, 0] < len(citymap)) &
-                               (nodes[:, 1] >= 0) & (nodes[:, 1] < len(citymap[0]))], axis=0)
+    city.mark_antinodes(city.antinodes2)
+    city.pp()
 
-    data = fname.read().strip()
-    citymap = [list(row) for row in data.split()]
-    freqs = defaultdict(list)
-    for r,row in enumerate(citymap):
-        for c,cell in enumerate(row):
-            if cell != '.' and cell != '#':
-                freqs[cell].append(np.array([r,c]))
-
-    antinodes = np.array([p[1] + ((p[1] - p[0]) * mults) for v in freqs.values() for p in combinations(v,2) for mults in (1,-2)])
-    antinodes = clip_to_city_limits(antinodes)
-
-    diag = len(citymap) + len(citymap[0])
-    antinodes2 = np.array([p[1] + ((p[1] - p[0]) * mults) for v in freqs.values() for p in combinations(v,2) for mults in range(-diag,diag)])
-    antinodes2 = clip_to_city_limits(antinodes2)
-
-    for p in antinodes2:
-        if citymap[p[0]][p[1]] == '.':
-            citymap[p[0]][p[1]] = '#'
-    pp_map()
-
-    print (f'Part 1: {len(antinodes)}')
-    print (f'Part 1: {len(antinodes2)}')
+    print (f'Part 1: {len(city.antinodes)}')
+    print (f'Part 1: {len(city.antinodes2)}')
 
 if __name__ == "__main__":
     main(open(sys.argv[1], encoding="utf-8") if len(sys.argv) > 1 else sys.stdin)
