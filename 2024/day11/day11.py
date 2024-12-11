39280241f30054818e7d7a8166f0fceb3700747c
Olaf Brandt <olaf.brandt@gmail.com>
2024-12-10 22:31:16 -0800

    additional cleanup

--- 2024/day11/day11.py
+++ 2024/day11/day11.py
@@ -14,15 +14,15 @@ class PlutonianPebbles:
         stones = Counter()
         for n, cnt in self.stones.items():
             if n == 0:
-                stones[1] = stones[1] + cnt
+                stones[1] += cnt
             else:
                 d = int(log10(n)+1)
                 if d & 1 == 0:
                     a, b = divmod(n,10**(d//2))
-                    stones[a] = stones[a] + cnt
-                    stones[b] = stones[b] + cnt
+                    stones[a] += cnt
+                    stones[b] += cnt
                 else:
-                    stones[n * 2024] = stones[n * 2024] + cnt
+                    stones[n * 2024] += cnt
         self.stones = stones
 
     def total(self) -> int:
@@ -33,7 +33,7 @@ def main(fname) -> None:
     for i, count in enumerate([25,50]):
         for _ in range(count):
             pp.blink()
-        print (f'Part {i+1}: {pp.total()}')
+        print (f'Part {i+1}: {pp.total()}  {len(pp.stones.items())=}')
 
 if __name__ == "__main__":
     main(open(sys.argv[1], encoding="utf-8") if len(sys.argv) > 1 else sys.stdin)
