f736f21c5c646dae0a3a63b97be20331efb64cea
Olaf Brandt <olaf.brandt@gmail.com>
2024-12-12 18:12:19 -0800

    oo-ify

--- 2024/day12/day12.py
+++ 2024/day12/day12.py
@@ -5,79 +5,60 @@
 import sys
 from collections import defaultdict
 from collections import deque
+from functools import reduce
+from operator import or_
 
 class GardenGroups:
-    def __init__(self):
-        pass
-
-def main(fname) -> None:
+    def __init__(self, data):
+        self.grid = {(r,c): ch for r,row in enumerate(data.split()) for c,ch in enumerate(row)}
+        self.areas = self.find_areas()
+        self.perims = {k:sum(4 - self.friends(p) for p in v) for k,v in self.areas.items()}
+        self.sides = {k:self.count_sides(v) for k,v in self.areas.items()}
 
     dirs = [(-1,0),(0,-1),(1,0),(0,1)] # right / clockwise
 
-    def tuple_add(a:tuple[int,int], b:tuple[int,int]) -> tuple[int,int]:
-        return (a[0]+b[0],a[1]+b[1])
+    @staticmethod
+    def tuple_add(a:tuple, b:tuple) -> tuple:
+        return tuple([a+b for a,b in zip(a,b)])
 
-    def matching_neigbors(p):
-        return sum(grid[p] == grid[tuple_add(p,d)] for d in dirs if tuple_add(p,d) in grid)   
+    def friends(self, p) -> int:
+        return sum(self.grid[p] == self.grid[self.tuple_add(p,d)] for d in self.dirs if self.tuple_add(p,d) in self.grid)
 
-    def count_sides(area) -> int:
-        horz = dict()
-        vert = dict()
-        sides = 0
-
-        for p in area:
-            up,left,down,right = [tuple_add(p,d) for d in dirs]
-            if left not in area:
-                horz[p] = 1
-            if right not in area:
-                horz[right] = 2
-            if up not in area:
-                vert[p] = 1
-            if down not in area:
-                vert[down] = 2
-        sides += sum([v != horz.get((k[0]-1,k[1]), 0) for k,v in horz.items()])
-        sides += sum([v != vert.get((k[0],k[1]-1), 0) for k,v in vert.items()])
+    def count_sides(self, area) -> int:
+        mask = {p: reduce(or_, [(1 << i) for i,d in enumerate(self.dirs) if self.tuple_add(p,d) not in area], 0) for p in area}
+        sides =  sum([v & bit != mask.get((k[0]-1,k[1]-0), 0) & bit for bit in [1<<1,1<<3] for k,v in mask.items()])
+        sides += sum([v & bit != mask.get((k[0]-0,k[1]-1), 0) & bit for bit in [1<<0,1<<2] for k,v in mask.items()])
         return sides
 
-
-    grid = {(r,c): ch for r,row in enumerate(fname.read().split()) for c,ch in enumerate(row)}
-    H = max(k[0] for k in grid.keys())+1
-    W = max(k[1] for k in grid.keys())+1    
-    areas = defaultdict(set)
-    visited = {}
-
-    for r in range(H):
-        for c in range(W):
-            root = (r,c)
+    def find_areas(self) -> defaultdict:
+        areas = defaultdict(set)
+        visited = set()
+        for root in self.grid.keys():
             if root in visited:
                 continue
-            ch = grid[root]
+            ch = self.grid[root]
             q = deque([root])
             while q:
                 p = q.popleft()
                 if p in visited:
                     continue
-                q.extend(x for d in dirs for x in [tuple_add(p,d)] if x in grid and grid[x] == ch and x not in visited)
-                visited[p] = True
+                q.extend(x for d in self.dirs for x in [self.tuple_add(p,d)] if x in self.grid and self.grid[x] == ch and x not in visited)
+                visited.add(p)
                 areas[root].add(p)
-    
-    perims = {k:sum(4 - matching_neigbors(p) for p in v) for k,v in areas.items()}
-    sides = {k:count_sides(v) for k,v in areas.items()}
+        return areas
 
-    prices1 = []
-    for k,v in areas.items():
-        prices1.append(len(v) * perims[k])
-        #print (f'A region of {grid[k]} plants with price {len(v)} * {perims[k]} = {prices1[-1]}.')
+    def price_list(self, factor, verbose=False) -> int:
+        prices = []
+        for k,v in self.areas.items():
+            prices.append(len(v) * factor[k])
+            if verbose:
+                print (f'A region of {self.grid[k]} plants with price {len(v)} * {factor[k]} = {prices[-1]}.')
+        return sum(prices)
 
-    print (f'Part 1: {sum(prices1)}')
-
-    prices2 = []
-    for k,v in areas.items():
-        prices2.append(len(v) * sides[k])
-        #print (f'A region of {grid[k]} plants with price {len(v)} * {sides[k]} = {prices2[-1]}.')
-
-    print (f'Part 2: {sum(prices2)}')    
-
+def main(fname) -> None:
+    gg = GardenGroups(fname.read())
+    print (f'Part 1: {gg.price_list(gg.perims, True)}')
+    print (f'Part 2: {gg.price_list(gg.sides, True)}')
 
 if __name__ == "__main__":
     main(open(sys.argv[1], encoding="utf-8") if len(sys.argv) > 1 else sys.stdin)
