4188ddf4b7214cb54ada5bcd6b9d74127565eedc
Zachary Brandt <zachary.brandt@gmail.com>
2024-12-22 23:34:22 -0800

    Day 9 work

---
+++ 2024/day09.py
@@ -0,0 +1,33 @@
+import sys
+
+line = sys.stdin.read()
+blocks = ""
+count = 0
+
+index = 0
+for i in range(len(line)):
+    if i % 2 == 0:
+        blocks += int(line[i]) * str(index)
+        index += 1
+    else:
+        blocks += int(line[i]) * '.'
+
+def check(chars):
+    for i in reversed(range(len(chars))):
+        if chars[i] != '.':
+            return i
+
+chars = list(blocks)
+i, j = blocks.index('.'), len(blocks) - 1
+while i <= j:
+    print(i, j)
+    chars[i], chars[j] = chars[j], chars[i]
+    i = chars.index('.')
+    j = check(chars)
+
+print(blocks)
+# print(''.join(chars))
+for i in range(chars.index('.')):
+    if chars[i] != '.':
+        count += i * int(chars[i])
+print(count)
