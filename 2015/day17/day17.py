60f7012c1dd2ee2897578e1d6ea8d1a7298436f2
Olaf Brandt <olaf.brandt@gmail.com>
2025-01-01 14:52:09 -0800

    nits

--- 2015/day17/day17.py
+++ 2015/day17/day17.py
@@ -7,19 +7,18 @@ import re
 
 def main(fname):
 
-    def count (target, buckets, used=0, depth=0) -> int:
+    def count (target, buckets, used=0) -> int:
         if target == 0:
             return [used]
         total = []
         if buckets:
-            bucket = list(buckets.items())[0]
-            if bucket[1] <= target:
-                total.extend(count(target - bucket[1], dict(list(buckets.items())[1:]), used+1, depth+1))
-            total.extend(count(target, dict(list(buckets.items())[1:]), used, depth+1))
+            if buckets[0] <= target:
+                total.extend(count(target - buckets[0], buckets[1:], used+1))
+            total.extend(count(target, buckets[1:], used))
         return total
 
-    buckets = {chr(ord('A')+i):int(vol) for i,vol in enumerate(re.findall(r'(\d+)',fname.read()))}
-    target = (25,150)[max(buckets.values()) > 25]
+    buckets = [int(vol) for vol in re.findall(r'(\d+)',fname.read())]
+    target = (25,150)[max(buckets) > 25]
 
     counts = count(target, buckets)
     minb = min(counts)
