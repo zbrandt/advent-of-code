6be113715712106af5480f666fd508bd7c4deffa
Olaf Brandt <olaf.brandt@gmail.com>
2025-01-06 14:02:15 -0800

    day07 part 2

--- 2016/day07/day07.py
+++ 2016/day07/day07.py
@@ -4,23 +4,25 @@
 # pylint: disable=line-too-long, missing-function-docstring, missing-class-docstring, redefined-builtin
 import sys
 import re
-"""
-([a-z]?=[a-z]{3})(?!\1)\2\1
-"""
+
 def main(fname):
-    lines_raw = fname.read().strip().split('\n')
-    lines = [re.sub(r'\[\w+\]','X',line) for line in lines_raw]
+    lines = fname.read().strip().split('\n')
+    lines_supernet = [re.sub(r'\[\w+\]','#',line) for line in lines]
+    lines_hypernet = ['#'.join(re.findall(r'\[(\w+)\]',line)) for line in lines]
 
-    #print (lines)
-    for i,line in enumerate(lines):
-        m = re.search(r'(([a-z])(?!\2)([a-z])\3\2)', line)
-        if m:
-            m = m.group(0)
-        #print (f'{lines_raw[i]} ==> {m}')
+    def match_count(x):
+        return len(re.findall(r'(([a-z])(?!\2)([a-z])\3\2)', x))
 
-    codes = [re.search(r'(([a-z])(?!\2)([a-z])\3\2)', line) is not None for line in lines]
-    print (codes)
+    def find_xyx(z, swap):
+        return {(m[0],re.sub(r'(([a-z])(?!\2)([a-z])\2)',r'\3\2\3', m[0]))[swap] for m in re.findall(r'(?=(([a-z])(?!\2)([a-z])\2))', z)}
+
+    matches = [ tuple(match_count(line[i]) for i in range(2)) for line in zip(lines, lines_supernet) ]
+    codes = [ m[1] > 0 and m[1] == m[0] for m in matches]
+
+    ssls = [bool(find_xyx(x[0], 0) & find_xyx(x[1], 1)) for x in zip(lines_supernet, lines_hypernet)]
+
     print (f'Part 1: {sum(codes)}')
+    print (f'Part 2: {sum(ssls)}')
 
 if __name__ == "__main__":
     main(open(sys.argv[1], encoding="utf-8") if len(sys.argv) > 1 else sys.stdin)
